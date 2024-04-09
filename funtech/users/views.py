0a27cf31cd815ffe02c61efd30d0027ee5e603b1
Денис Малич <malichdenis09@mail.ru>
2024-04-09 17:00:51 +0300

    yandexID url redirect

--- funtech/users/views.py
+++ funtech/users/views.py
@@ -16,14 +16,16 @@ class RegisterUser(APIView):
 class CreateToken(APIView):
     """Get token API."""
 
-    def post(self, request):
-        serializer = '()'                        ###########
-        serializer.is_valid(raise_exception=True)
-        code = serializer.validated_data['code']
+    def get(self, request):
+        print(request)
+        print(request.args)
+        if request.args.get('code'):
+            code = request.args.get('code')
+
         res = requests.post(f'https://oauth.yandex.ru/token', data={'grant_type': 'authorization_code',
                                                             'code': code,
-                                                            'client_id': '6e05c91a25f74e4c8661025fc46baa2b',
-                                                            'client_secret': '25665478fb3644edb43b3246199dd327'})
+                                                            'client_id': '6e0534534dffdfg',
+                                                            'client_secret': '2566dfgdfgdfgd345345345345'})
         if "error" in res:
             return Response({"error": res["error"]}, status=status.HTTP_400_BAD_REQUEST)
         # Token creation!
