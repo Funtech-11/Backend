23372529b011f5e9c8fc89e6ecbdf81c50013949
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2024-04-26 13:14:51 +0300

    Autocreate issues, new migrate, etc.
        - Added action for create issues automatically from TODO comments
        - Created migrate with phone validator
        - Removed creating superuser in entrypoint file

--- funtech/entrypoint.sh
+++ funtech/entrypoint.sh
@@ -3,10 +3,6 @@
 sleep 15
 
 python manage.py migrate
-
-source ./.env
-echo "from users.models import User; User.objects.create_superuser(username='$ADMIN_USERNAME', first_name='$ADMIN_FIRST_NAME', last_name='$ADMIN_LAST_NAME', password='$ADMIN_PASSWORD')" | python manage.py shell
-
 python manage.py load_data
 
 gunicorn funtech.wsgi:application --bind 0:8000
